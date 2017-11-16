3e6fb9b2c7724dcdc1f6a20b68b5e9772cce6d66
bj0ris <btthunem@gmx.com>
2017-11-16 17:17:38 +0100

    Updated the script a bit and added some poorly made marker images

--- script.js
+++ script.js
@@ -2,7 +2,12 @@
 //alert("JS is working");
 
 var table = document.getElementsByTagName('table')[0];
+//takenElArray is an array of which spots has been taken
 var takenElArray = [];
+var playerImage = document.getElementById('playerImage');
+var player = 'x';  //?????? Maybe not have it as a global?
+
+changePlayer();
 
 table.addEventListener('click',function(e){
     checkIfTaken(e.target);
@@ -14,10 +19,35 @@ function checkIfTaken(targetEl){
     }
     else{
         takenElArray.push(targetEl);
+        console.log("Unkown player took "+targetEl);
+        addMarker(targetEl);
         checkVictory();
+        changePlayer();
     }
 }
 
 function checkVictory(){
     //TODO
+}
+
+function changePlayer(){
+    if(player==='x'){
+        player = 'o';
+        playerImage.setAttribute('src','./images/O.png');
+    }
+    else{
+        player = 'x';
+        playerImage.setAttribute('src','./images/X.png');
+    }
+}
+
+function addMarker(targetEl){
+    var newImage = document.createElement('img');
+    if(player==='x'){
+        newImage.setAttribute('src','./images/X.png');
+    }
+    else{
+        newImage.setAttribute('src','./images/O.png');
+    }
+    targetEl.appendChild(newImage);
 }
